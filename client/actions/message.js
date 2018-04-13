682693598fdfbbd98f13efa9763a28358db70b43
trejgun <trejgun@gmail.com>
2018-04-13 18:59:19 +0800

    spaces

--- client/actions/message.js
+++ client/actions/message.js
@@ -2,15 +2,15 @@ import {MESSAGE_ADD, MESSAGE_REMOVE} from "../../shared/constants/actions";
 
 
 export const messageShow = data =>
-	dispatch =>
-		dispatch({
-			data,
-			type: MESSAGE_ADD
-		});
+  dispatch =>
+    dispatch({
+      data,
+      type: MESSAGE_ADD
+    });
 
 export const messageRemove = data =>
-	dispatch =>
-		dispatch({
-			data,
-			type: MESSAGE_REMOVE
-		});
+  dispatch =>
+    dispatch({
+      data,
+      type: MESSAGE_REMOVE
+    });
