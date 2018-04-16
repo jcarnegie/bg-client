8bec12f3df22fc935455729f487ba4c6d1fe64aa
trejgun <trejgun@gmail.com>
2018-04-16 10:17:31 +0800

    dont show popup before account is loaded

--- client/reducers/account.js
+++ client/reducers/account.js
@@ -1,11 +1,15 @@
 const account = {
+  isLoaded: false,
   wallet: null
 };
 
 export default function hashReducer(state = account, action) {
   switch (action.type) {
     case "CHANGE_ACCOUNT":
-      return Object.assign({}, state, {wallet: action.payload.wallet});
+      return Object.assign({}, state, {
+        isLoaded: true,
+        wallet: action.payload.wallet
+      });
     default:
       return state;
   }
