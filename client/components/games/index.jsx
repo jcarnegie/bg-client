0a6c86aef0b6a9739db5dd13fb685128098f6f6b
trejgun <trejgun@gmail.com>
2018-04-17 14:50:06 +0800

    add param in url to change iframe url

--- client/components/games/index.jsx
+++ client/components/games/index.jsx
@@ -1,14 +1,41 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
+import {withRouter} from "react-router";
 import Init from "../common/init";
+import {readFromQueryString} from "../../utils/location";
 
 
+@withRouter
 export default class Games extends Component {
+  static propTypes = {
+    location: PropTypes.shape({
+      search: PropTypes.string.isRequired
+    }).isRequired
+  };
+
+  state = {
+    url: ""
+  };
+
+  componentDidMount() {
+    this.setState({
+      url: readFromQueryString("url", this.props.location.search) || "https://bitguild.info/"
+    });
+  }
+
+  renderIframe() {
+    if (!this.state.url) {
+      return null;
+    }
+    return (<iframe src={this.state.url} />);
+  }
+
   render() {
     return (
       <div>
         <Init />
         <h2>Games</h2>
-        <iframe src="https://bitguild.info/" />
+        {this.renderIframe()}
       </div>
     );
   }
