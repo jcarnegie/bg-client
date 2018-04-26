e6ec3a7cb519317b33c2083d5c73601b01fdbdba
trejgun <trejgun@gmail.com>
2018-04-26 11:00:41 +0800

    simplify error handling

--- client/components/common/message.jsx
+++ client/components/common/message.jsx
@@ -1,22 +1,20 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import {FormattedMessage} from "react-intl";
-import {messageRemove} from "../../actions/message";
 import {Alert} from "react-bootstrap";
+import {MESSAGE_REMOVE_ALL, MESSAGE_REMOVE} from "../../../shared/constants/actions";
 
 
 @connect(
   state => ({
     messages: state.messages
-  }),
-  dispatch => bindActionCreators({messageRemove}, dispatch)
+  })
 )
 export default class Message extends Component {
   static propTypes = {
     messages: PropTypes.array,
-    messageRemove: PropTypes.func
+    dispatch: PropTypes.func
   };
 
   static defaultProps = {
@@ -24,14 +22,17 @@ export default class Message extends Component {
   };
 
   componentWillUnmount() {
-    this.props.messages.map(message =>
-      this.props.messageRemove(message)
-    );
+    this.props.dispatch({
+      type: MESSAGE_REMOVE_ALL
+    });
   }
 
   onDismiss(message) {
     return () => {
-      this.props.messageRemove(message);
+      this.props.dispatch({
+        payload: message,
+        type: MESSAGE_REMOVE
+      });
     };
   }
 
