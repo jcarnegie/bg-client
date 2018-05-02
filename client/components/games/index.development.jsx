331e6114aae5f045abccc2b1db6c02600f535fea
trejgun <trejgun@gmail.com>
2018-05-02 08:59:53 +0800

    placeholder for game list

--- client/components/games/index.development.jsx
+++ client/components/games/index.development.jsx
@@ -1,11 +1,20 @@
+import "./games.less";
 import React, {Component} from "react";
+import {Image, Button} from "react-bootstrap";
 
 
 export default class Games extends Component {
   render() {
     return (
-      <div>
-        <h2>Games</h2>
+      <div className="games">
+        <Image src="/images/landing.png" />
+        <div className="title">BitSociety</div>
+        <div className="description">
+          BitSociety is an avatar creation game built into the BitGuild<br/>
+          portal that let's you find fantastic high-value items, show them<br/>
+          off to everyone, or barter them at the BitGuild marketplace.<br/>
+        </div>
+        <Button />
       </div>
     );
   }
