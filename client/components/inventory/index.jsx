784a9cd906b5d819907ebf05381fe802ff79c19f
trejgun <trejgun@gmail.com>
2018-05-08 11:07:11 +0800

    empty item list

--- client/components/inventory/index.jsx
+++ client/components/inventory/index.jsx
@@ -1,12 +1,14 @@
 import "./index.less";
+import "./tabs.less";
 import React, {Component, Fragment} from "react";
-import {Button, Col, Row, Tab, Tabs} from "react-bootstrap";
+import {Button, Col, Image, Row, Tab, Tabs} from "react-bootstrap";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import Loader from "../common/loader";
 import Item from "./item";
 import {uniq, uniqBy} from "lodash";
 import {FormattedMessage} from "react-intl";
+import {Link} from "react-router-dom";
 
 
 @connect(
@@ -54,45 +56,39 @@ export default class Inventory extends Component {
       return null;
     }
 
+    return (
+      <Row className="inventory">
+        <Col className="grap gap">
+          {inventory.data.length ? this.renderTabs() : this.renderEmpty()}
+        </Col>
+        <Col className="chat">
+          Chat
+        </Col>
+      </Row>
+    );
+  }
+
+  renderTabs() {
+    const {inventory} = this.props;
     const games = uniqBy(inventory.data.map(item => item.game), "_id");
 
     return (
-      <Row>
-        <Col className="grap gap">
-          <h2>
-            <FormattedMessage id="pages.inventory.title" />
-            <div className="pull-right">
-              <Button>
-                <FormattedMessage id="pages.inventory.back-to-game" />
-              </Button>
-            </div>
-          </h2>
-
-          <Tabs defaultActiveKey={1} id="inventory" onSelect={::this.onSelect}>
-            <Tab eventKey={1} title={<FormattedMessage id="pages.inventory.all-items" />}>
-              {games.map(game => {
-                const items = inventory.data.filter(item => item.game._id === game._id);
-                const categories = uniq([].concat(...items.map(item => item.categories)));
-                return (
-                  <Fragment key={game._id}>
-                    <div className="arrow-right pull-right">
-                      <Button onClick={::this.onClick(game, categories)} bsStyle="link">All</Button>
-                      {categories.map((category, i) => <Button key={i} onClick={::this.onClick(game, [category])} bsStyle="link">{category}</Button>)}
-                    </div>
-                    <h3>{game.name}</h3>
-                    <Row>
-                      {items.filter(item => Object.keys(this.state.filters).includes(item.game._id) ? this.state.filters[item.game._id].filter(x => !!~item.categories.indexOf(x)).length : true)
-                        .map(item => <Item key={item._id} {...item} onClick={::this.onClick} />)}
-                    </Row>
-                  </Fragment>
-                );
-              })}
-            </Tab>
-            {games.map((game, i) => {
+      <>
+        <h2>
+          <FormattedMessage id="pages.inventory.title" />
+          <div className="pull-right">
+            <Button>
+              <FormattedMessage id="pages.inventory.back-to-game" />
+            </Button>
+          </div>
+        </h2>
+        <Tabs defaultActiveKey={1} id="inventory" onSelect={::this.onSelect}>
+          <Tab eventKey={1} title={<FormattedMessage id="pages.inventory.all-items" />}>
+            {games.map(game => {
               const items = inventory.data.filter(item => item.game._id === game._id);
               const categories = uniq([].concat(...items.map(item => item.categories)));
               return (
-                <Tab eventKey={i + 2} title={game.name} key={game._id}>
+                <Fragment key={game._id}>
                   <div className="arrow-right pull-right">
                     <Button onClick={::this.onClick(game, categories)} bsStyle="link">All</Button>
                     {categories.map((category, i) => <Button key={i} onClick={::this.onClick(game, [category])} bsStyle="link">{category}</Button>)}
@@ -102,15 +98,49 @@ export default class Inventory extends Component {
                     {items.filter(item => Object.keys(this.state.filters).includes(item.game._id) ? this.state.filters[item.game._id].filter(x => !!~item.categories.indexOf(x)).length : true)
                       .map(item => <Item key={item._id} {...item} onClick={::this.onClick} />)}
                   </Row>
-                </Tab>
+                </Fragment>
               );
             })}
-          </Tabs>
-        </Col>
-        <Col className="chat">
-          Chat
-        </Col>
-      </Row>
+          </Tab>
+          {games.map((game, i) => {
+            const items = inventory.data.filter(item => item.game._id === game._id);
+            const categories = uniq([].concat(...items.map(item => item.categories)));
+            return (
+              <Tab eventKey={i + 2} title={game.name} key={game._id}>
+                <div className="arrow-right pull-right">
+                  <Button onClick={::this.onClick(game, categories)} bsStyle="link">All</Button>
+                  {categories.map((category, i) => <Button key={i} onClick={::this.onClick(game, [category])} bsStyle="link">{category}</Button>)}
+                </div>
+                <h3>{game.name}</h3>
+                <Row>
+                  {items.filter(item => Object.keys(this.state.filters).includes(item.game._id) ? this.state.filters[item.game._id].filter(x => !!~item.categories.indexOf(x)).length : true)
+                    .map(item => <Item key={item._id} {...item} onClick={::this.onClick} />)}
+                </Row>
+              </Tab>
+            );
+          })}
+        </Tabs>
+      </>
+    );
+  }
+
+  renderEmpty() {
+    return (
+      <div className="empty">
+        <div>
+          <h2>
+            <FormattedMessage id="pages.inventory.empty" />
+          </h2>
+          <Image src="/images/empty-box.png" />
+          <p>
+            <FormattedMessage id="pages.inventory.learn-more" />
+            {" "}
+            <Link to="/faq">
+              <FormattedMessage id="pages.inventory.faq" />
+            </Link>
+          </p>
+        </div>
+      </div>
     );
   }
 }
