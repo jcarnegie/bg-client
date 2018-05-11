21550c58202ee7f9f0ec9d3f32da7517f5b2fe70
trejgun <trejgun@gmail.com>
2018-05-11 09:37:36 +0800

    remove hacky hack beacause we have proper staging

--- build.sh
+++ build.sh
@@ -7,13 +7,6 @@ node_modules/.bin/babel -d ./build/shared ./shared --copy-files
 node_modules/.bin/babel -d ./build/static ./static --copy-files
 node_modules/.bin/babel -d ./build/client ./client --copy-files
 
-
-# there is no way to determinate domain on server side rendering
-# i.e i can't pass request.headers.host to window.document.location.host
-# so i'm switching to client site rendering until we have proper staging
-
-
-# TODO switch back to `server` once we have proper staging
 export RENDERING=server
 export NODE_ENV=production
 export MAINNET_TOKEN_CONTRACT_ADDR=0x7E43581b19ab509BCF9397a2eFd1ab10233f27dE
