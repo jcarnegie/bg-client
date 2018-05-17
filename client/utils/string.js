9a0c9476c48a35b0eaf4d53aa5293f42d041b68e
Jeff Carnegie <jeff.carnegie@gmail.com>
2018-05-17 16:41:18 -0700

    Guard against empty item attribute values

--- client/utils/string.js
+++ client/utils/string.js
@@ -1,6 +1,7 @@
-import {any, curry, isEmpty, match, not} from "ramda";
+import {any, curry, isEmpty, isNil, match, not} from "ramda";
 
 export const matchesOneOf = curry((matchers, str) => {
+  if (isNil(str)) return false;
   const doesMatch = curry((s, re) => not(isEmpty(match(re, s))));
   return any(doesMatch(str), matchers);
 });
