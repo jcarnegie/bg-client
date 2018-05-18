2a99d7c9d2c281634058c4ac30cee44d3d603c7b
TrejGun <TrejGun@users.noreply.github.com>
2018-05-19 01:34:49 +0800

    fix html markup in i18n on faq page (#34)

--- client/components/faq/index.jsx
+++ client/components/faq/index.jsx
@@ -1,6 +1,6 @@
 import "./index.less";
 import React, {Component} from "react";
-import {FormattedMessage} from "react-intl";
+import {FormattedMessage, FormattedHTMLMessage} from "react-intl";
 import {Col, Row} from "react-bootstrap";
 import Chat from "../chat/chat";
 
@@ -15,81 +15,81 @@ export default class Faq extends Component {
           <h3><FormattedMessage id="pages.faq.metamask.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.metamask.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.metamask.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.metamask.a1" /></dd>
             <dt><FormattedMessage id="pages.faq.metamask.q2" /></dt>
-            <dd><FormattedMessage id="pages.faq.metamask.a2" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.metamask.a2" /></dd>
             <dt><FormattedMessage id="pages.faq.metamask.q3" /></dt>
-            <dd><FormattedMessage id="pages.faq.metamask.a3" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.metamask.a3" /></dd>
             <dt><FormattedMessage id="pages.faq.metamask.q4" /></dt>
-            <dd><FormattedMessage id="pages.faq.metamask.a4" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.metamask.a4" /></dd>
           </dl>
 
           <h3><FormattedMessage id="pages.faq.plat.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.plat.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.plat.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.plat.a1" /></dd>
             <dt><FormattedMessage id="pages.faq.plat.q2" /></dt>
-            <dd><FormattedMessage id="pages.faq.plat.a2" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.plat.a2" /></dd>
             <dt><FormattedMessage id="pages.faq.plat.q3" /></dt>
-            <dd><FormattedMessage id="pages.faq.plat.a3" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.plat.a3" /></dd>
             <dt><FormattedMessage id="pages.faq.plat.q4" /></dt>
-            <dd><FormattedMessage id="pages.faq.plat.a4" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.plat.a4" /></dd>
           </dl>
 
           <h3><FormattedMessage id="pages.faq.games.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.games.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.games.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.games.a1" /></dd>
             <dt><FormattedMessage id="pages.faq.games.q2" /></dt>
-            <dd><FormattedMessage id="pages.faq.games.a2" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.games.a2" /></dd>
           </dl>
 
           <h3><FormattedMessage id="pages.faq.items.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.items.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a1" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q2" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a2" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a2" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q3" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a3" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a3" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q4" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a4" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a4" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q5" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a5" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a5" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q6" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a6" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a6" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q7" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a7" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a7" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q8" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a8" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a8" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q9" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a9" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a9" /></dd>
             <dt><FormattedMessage id="pages.faq.items.q10" /></dt>
-            <dd><FormattedMessage id="pages.faq.items.a10" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.items.a10" /></dd>
           </dl>
 
           <h3><FormattedMessage id="pages.faq.games.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.account.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.account.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.account.a1" /></dd>
             <dt><FormattedMessage id="pages.faq.account.q2" /></dt>
-            <dd><FormattedMessage id="pages.faq.account.a2" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.account.a2" /></dd>
             <dt><FormattedMessage id="pages.faq.account.q3" /></dt>
-            <dd><FormattedMessage id="pages.faq.account.a3" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.account.a3" /></dd>
           </dl>
 
           <h3><FormattedMessage id="pages.faq.community.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.community.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.community.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.community.a1" /></dd>
             <dt><FormattedMessage id="pages.faq.community.q2" /></dt>
-            <dd><FormattedMessage id="pages.faq.community.a2" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.community.a2" /></dd>
           </dl>
 
           <h3><FormattedMessage id="pages.faq.developers.title" /></h3>
           <dl>
             <dt><FormattedMessage id="pages.faq.developers.q1" /></dt>
-            <dd><FormattedMessage id="pages.faq.developers.a1" /></dd>
+            <dd><FormattedHTMLMessage id="pages.faq.developers.a1" /></dd>
           </dl>
         </Col>
         <Col className="chat">
