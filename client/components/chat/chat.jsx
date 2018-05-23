60506d7f11ce23724bee3c1bddf22f28a95d5122
TrejGun <TrejGun@users.noreply.github.com>
2018-05-24 01:22:44 +0800

    move inline css to less (#53)
    cleanup markup

--- client/components/chat/chat.jsx
+++ client/components/chat/chat.jsx
@@ -3,21 +3,12 @@ import PropTypes from "prop-types";
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import StayScrolled from "react-stay-scrolled";
-import {isEmpty, map} from "ramda";
+import {isEmpty} from "ramda";
 import {sendChatMessage} from "../../actions/chat";
 import Message from "./message";
 import {FormattedMessage} from "react-intl";
+import {Button, Form, FormControl, FormGroup} from "react-bootstrap";
 
-
-const styles = {
-  container: {backgroundColor: "#F2F3F8", borderLeft: "solid 1px #C2C3D2", display: "flex", flexDirection: "column", height: "calc(100vh - 62px)"},
-  form: {display: "flex", width: "auto"},
-  header: {alignItems: "center", backgroundColor: "#DEE6F4", color: "#8AA0C8", display: "flex", fontSize: 14, height: 32, justifyContent: "center", minHeight: 32},
-  messageInput: {border: "none", borderBottom: "solid 1px #D1D1D1", flex: 1, outline: "none", margin: "10px 10px 15px 10px"},
-  messageInputWrapper: {backgroundColor: "#FFF", borderTop: "solid 1px #E5E5EB", display: "flex", flexGrow: 6},
-  messageButton: {backgroundColor: "#4D4D83", borderRadius: 3, color: "#F9F9FB", flexGrow: 1, padding: "15px 10px"},
-  messageList: {flexGrow: 1, overflowY: "scroll", paddingBottom: "10px"}
-};
 
 @connect(
   state => ({
@@ -46,7 +37,9 @@ export default class Chat extends Component {
 
     e.preventDefault();
 
-    if (isEmpty(newMessage.trim())) return;
+    if (isEmpty(newMessage.trim())) {
+      return;
+    }
 
     sendChatMessage(this.state.newMessage);
     // clear the message input
@@ -63,28 +56,24 @@ export default class Chat extends Component {
   };
 
   render() {
-    const {messages} = this.props.chat;
-    const {user} = this.props;
+    const {user, chat} = this.props;
+
     return (
-      <div style={styles.container}>
-        <div style={styles.header}>
-          <div>
-            <FormattedMessage id="chat.chat" />
-          </div>
+      <div className="wrapper">
+        <div className="top">
+          <FormattedMessage id="chat.chat" />
         </div>
-        <StayScrolled component="div" provideControllers={this.storeScrolledControllers} style={styles.messageList}>
-          {map(msg => <Message key={msg.messageId} message={msg} user={user} />, messages)}
+        <StayScrolled component="div" provideControllers={this.storeScrolledControllers} className="list">
+          {chat.messages.map(msg => <Message key={msg.messageId} message={msg} user={user} />)}
         </StayScrolled>
-        <div>
-          <form onSubmit={::this.handleSubmit} style={styles.form}>
-            <div style={styles.messageInputWrapper}>
-              <input onChange={::this.handleMessageChange} style={styles.messageInput} type="text" value={this.state.newMessage} />
-            </div>
-            <button style={styles.messageButton}>
+        <Form onSubmit={::this.handleSubmit}>
+          <FormGroup>
+            <FormControl onChange={::this.handleMessageChange} type="text" value={this.state.newMessage} />
+            <Button type="submit">
               <FormattedMessage id="chat.send" />
-            </button>
-          </form>
-        </div>
+            </Button>
+          </FormGroup>
+        </Form>
       </div>
     );
   }
