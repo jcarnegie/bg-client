bb9428079c1dc5a5967a94bd4b2677ad61a6e60a
Shain <srslafazan@gmail.com>
2018-05-24 11:47:20 -0700

    Sanitized chat messages and added link support

--- client/components/chat/message.jsx
+++ client/components/chat/message.jsx
@@ -1,9 +1,47 @@
 import "./message.less";
 import PropTypes from "prop-types";
+import sanitizeHtml from "sanitize-html";
 import React, {Component} from "react";
 import {path} from "ramda";
 import {Image} from "react-bootstrap";
 
+
+const reWebUrl = new RegExp(
+  "^" +
+    // protocol identifier
+    "(?:(?:https?|ftp)://)" +
+    // user:pass authentication
+    "(?:\\S+(?::\\S*)?@)?" +
+    "(?:" +
+      // IP address exclusion
+      // private & local networks
+      "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+      "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+      "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+      // IP address dotted notation octets
+      // excludes loopback network 0.0.0.0
+      // excludes reserved space >= 224.0.0.0
+      // excludes network & broacast addresses
+      // (first & last IP address of each class)
+      "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+      "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+      "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+    "|" +
+      // host name
+      "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+      // domain name
+      "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+      // TLD identifier
+      "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+      // TLD may end with dot
+      "\\.?" +
+    ")" +
+    // port number
+    "(?::\\d{2,5})?" +
+    // resource path
+    "(?:[/?#]\\S*)?" +
+  "$", "ig"
+);
 
 const formatTime = date => {
   let hours = date.getHours();
@@ -26,13 +64,26 @@ export default class Message extends Component {
     user: PropTypes.object
   };
 
-  renderAvarar(flag) {
+  renderAvatar(flag) {
     if (!flag) {
       return null;
     }
 
     return (
       <Image src="/images/avatar.png" width="34" height="34" />
+    );
+  }
+
+  createMessage(textColor = "#393939") {
+    const {message} = this.props;
+    let msg = "";
+    msg = message.message.replace(reWebUrl, url => `<a href="${url}" target="_blank">${url}</a>`);
+    msg = sanitizeHtml(msg);
+    return (
+      <div
+        className="contents"
+        dangerouslySetInnerHTML={{__html: msg}}
+      />
     );
   }
 
@@ -44,17 +95,17 @@ export default class Message extends Component {
     return (
       <div className="message">
         <div className="avatar">
-          {this.renderAvarar(!isMyMessage)}
+          {this.renderAvatar(!isMyMessage)}
         </div>
         <div className={"message-box" + (isMyMessage ? " my" : "")}>
           <div className="header">
             <div>{path(["sender", "nickname"], message)}</div>
             <div>{formatTime(new Date(message.createdAt))}</div>
           </div>
-          <div className="body">{message.message}</div>
+          <div className="body">{this.createMessage()}</div>
         </div>
         <div className="avatar">
-          {this.renderAvarar(isMyMessage)}
+          {this.renderAvatar(isMyMessage)}
         </div>
       </div>
     );
--- client/sagas/chat.js
+++ client/sagas/chat.js
@@ -1,4 +1,8 @@
 import {put, select, takeEvery} from "redux-saga/effects";
+import sanitizeHtml from "sanitize-html";
+import {
+  isEmpty,
+} from "ramda";
 import {channels as chatChannels,
   init as chatInit,
   messages as chatMessages,
@@ -15,10 +19,17 @@ function * sendChatMessage(action) {
     const state = yield select();
     const {currentChannel} = state.chat;
     const message = action.payload;
+
+    const cleanMsg = sanitizeHtml(message);
+    if (isEmpty(cleanMsg.trim())) {
+      return null;
+    }
+
     const sentMessage = yield sendMessage(message, currentChannel);
+
     yield put({
       type: CHAT_MESSAGE_SENT,
-      payload: sentMessage
+      payload: sentMessage,
     });
   } catch (e) {
     console.log("sendChatMessage error:", e);
