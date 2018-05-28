2f73c703e135cee663a292f0022b371441dd7e51
Jeff Carnegie <jeff.carnegie@gmail.com>
2018-05-28 00:06:32 -0700

    Added node-fetch for backend apollo rendering

--- client/utils/apollo.js
+++ client/utils/apollo.js
@@ -1,5 +1,8 @@
 import ApolloClient from "apollo-boost";
 
-export const client = new ApolloClient({
-  uri: "http://localhost:4000"
-});
+if (typeof global !== "undefined") {
+  global.fetch = require("node-fetch");
+}
+
+const uri = "http://localhost:4000";
+export const client = new ApolloClient({uri});
