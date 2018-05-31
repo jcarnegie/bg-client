7da8306da2f3a19a6006fec03fbffb46cc47509c
Jeff Carnegie <jeff.carnegie@gmail.com>
2018-05-31 00:16:33 -0700

    Updated asset rendering for environments

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -4,12 +4,10 @@ import webpack from "../configs/webpack";
 
 const router = Router(); // eslint-disable-line new-cap
 
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  router.use("/bundle", express.static(path.join(__dirname, "../../bundle")));
-}
-
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
   router.use(webpack);
+} else {
+  router.use("/bundle", express.static(path.join(__dirname, "../../bundle")));
 }
 
 router.use("/", express.static(path.join(__dirname, "../../static")));
