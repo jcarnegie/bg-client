7acd8a93a323c1aa75a112f68c3805b6d1d82d58
trejgun <trejgun@gmail.com>
2018-06-08 08:35:23 +0800

    use port from env

--- client/utils/apollo.js
+++ client/utils/apollo.js
@@ -4,6 +4,6 @@ if (typeof global !== "undefined") {
   global.fetch = require("node-fetch");
 }
 
-export const uri = (process.env.NODE_ENV === "development" ? "http://localhost:7000" : "") + "/api";
+export const uri = (process.env.NODE_ENV === "development" ? `http://localhost:${process.env.PORT}` : "") + "/api";
 
 export const client = new ApolloClient({uri: `${uri}/`});
