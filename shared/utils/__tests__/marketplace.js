e1ff04ce4e40757bfd01d7989888ee97b9ddebf4
Shain <srslafazan@gmail.com>
2018-07-24 22:01:41 -0700

    Updated marketplace business logic

--- shared/utils/__tests__/marketplace.js
+++ shared/utils/__tests__/marketplace.js
@@ -1,15 +1,13 @@
 import {
+  MARKETPLACE_DEFAULT_ITEM_LISTING_DAYS,
   listingIsExpiredForItem,
 } from '@/shared/utils/marketplace';
 
 test('listingIsExpiredForItem', () => {
-  const sevenDaysFromNow = new Date().setTime(new Date().getTime() + (7 * 24 * 60 * 60 * 1000));
-  const expiredItem = {
-    saleExpiration: '2001-07-18T22:25:35.169Z',
-  };
-  const listedItem = {
-    saleExpiration: sevenDaysFromNow,
-  };
+  const sevenDaysFromNow = new Date().setTime(new Date().getTime() + (MARKETPLACE_DEFAULT_ITEM_LISTING_DAYS * 24 * 60 * 60 * 1000));
+  const manyYearsAgo = '2001-07-18T22:25:35.169Z';
+  const expiredItem = { saleExpiration: manyYearsAgo };
+  const listedItem = { saleExpiration: sevenDaysFromNow };
   expect(listingIsExpiredForItem(expiredItem)).toBe(true);
   expect(listingIsExpiredForItem(listedItem)).toBe(false);
 });
--- shared/utils/marketplace.js
+++ shared/utils/marketplace.js
@@ -2,4 +2,6 @@
  * Marketplace business logic
 **/
 
+export const MARKETPLACE_DEFAULT_ITEM_LISTING_DAYS = 7;
+
 export const listingIsExpiredForItem = item => (new Date(item.saleExpiration) <= new Date());
