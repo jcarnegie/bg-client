4a626f895bdd598beb4f0e2cfef5d1544807ec4b
Yong <yongpark.sd@gmail.com>
2018-08-03 15:37:45 -0700

    Changes from all games to active games for banner display on landing page. (#222)

--- components/landing/index.jsx
+++ components/landing/index.jsx
@@ -113,7 +113,7 @@ class GameList extends Component {
         `}</style>
         <Col>
           <Carousel interval={null} className="hero-carousel" defaultActiveIndex={2}>
-            {games.data.map((game, idx) => {
+            {games.active.map((game, idx) => {
               // TODO - database flags for game states (active|presale|development)
               // TODO - bitizens is coming soon, will need to update when launches
               return (
