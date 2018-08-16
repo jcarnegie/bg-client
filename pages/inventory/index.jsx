57070c4a89f80d0f77f312fe6d3a762256a7f06e
Shain Lafazan <srslafazan@gmail.com>
2018-08-15 21:37:49 -0700

    Removed route guards (#268)

--- pages/inventory/index.jsx
+++ pages/inventory/index.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
-import Router from 'next/router';
-
-import { compose, graphql } from 'react-apollo';
+import { compose } from 'react-apollo';
 
 import {
   viewUserByWalletQuery,
-  localQueries,
 } from '@/shared/utils/apollo';
 
 import { DesktopContent, MobileContent, DesktopLayout, MobileLayout } from '@/components/layouts';
@@ -16,35 +13,9 @@ import Chat from '@/components/chat';
 class InventoryPage extends Component {
   static getInitialProps = ctx => ({});
 
-  state = {
-    timeout: 3000,
-    interval: null,
-  }
-
-  componentDidMount() {
-    this.setState({
-      interval: setInterval(() => {
-        if (this.state.timeout < 0) {
-          window.clearInterval(this.state.interval);
-          this.setState({ timeout: 0, interval: null });
-        }
-        this.setState({ timeout: (this.state.timeout - 100) });
-      }, 100),
-    });
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.state.interval);
-  }
-
   render() {
-    const { user, root } = this.props;
-    const { network } = root;
-    if (user.loading || root.loading) return null;
-    if (((user.error || (!user.viewUserByWallet && !user.loading)) || (!network.supported && network.supported !== null)) && this.state.timeout <= 0) {
-      Router.replace('/');
-      return null;
-    };
+    const { user } = this.props;
+    if (user.loading) return null;
     return (
       <>
         <MobileLayout
@@ -60,7 +31,4 @@ class InventoryPage extends Component {
 };
 
 
-export default compose(
-  viewUserByWalletQuery,
-  graphql(localQueries.root, { name: 'root' })
-)(InventoryPage);
+export default compose(viewUserByWalletQuery)(InventoryPage);
