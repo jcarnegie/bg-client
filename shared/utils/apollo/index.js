91a1147eb8913656b8610f21aa7d47a9ff466213
Shain Lafazan <srslafazan@gmail.com>
2018-09-21 10:57:43 -0700

    Auth remove queries (#369)
    * Skip test for method that needs updating
    
    * Remove viewUserByWalletQuery
    
    * Remove listGamesQuery
    
    * Remove listMarketplaceItems query
    
    * Remove listItemsQuery
    
    * Remove viewGameBySlug query
    
    * Removed dead references

--- shared/utils/apollo/index.js
+++ shared/utils/apollo/index.js
@@ -1,13 +1,8 @@
-import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import * as log from 'loglevel';
 import {
-  merge,
-  pickAll,
   dissoc,
 } from 'ramda';
 
-import { getWeb3Wallet } from '@/shared/utils/network';
 import { client } from '@/shared/utils/apollo/withApollo';
 
 export { client } from '@/shared/utils/apollo/withApollo';
@@ -117,40 +112,6 @@ export const queries = {
       }
     }
   `,
-  listItems: gql`
-    query listItems($userId: ID!, $language: String!) {
-      listItems(userId: $userId, language: $language) {
-        id presale lan tokenId image name description attrs saleExpiration salePrice saleState lastOwner {id} categories game { id }
-      }
-    }
-  `,
-  listGames: gql`{
-    listGames {
-      id name slug url stagingUrl api nft itemsForSaleCount enabled comingSoon
-      bannerImage thumbnailImage categoryIcon config
-    }
-  }`,
-  viewUserByWallet: gql`
-    query viewUserByWallet($wallet: String!) {
-      viewUserByWallet(wallet: $wallet) {
-        id wallet nickName language wallets lastWalletUsed
-      }
-    }
-  `,
-  viewGameBySlug: gql`
-    query viewGameBySlug($slug: String!) {
-      viewGameBySlug(slug: $slug) {
-        id name slug url stagingUrl api nft
-      }
-    }
-  `,
-  listMarketplaceItems: gql`
-      query listMarketplaceItems($language: String, $userId: Int, $gameId: Int, $categories: [String], $andNotCategories: [String], $sort: Value) {
-        listMarketplaceItems(language: $language, userId: $userId, gameId: $gameId, categories: $categories, andNotCategories: $andNotCategories, sort: $sort) {
-        id lan tokenId name description image attrs categories salePrice saleExpiration saleState lastOwner {id} saleExpiration saleCurrency game {id} user {id}
-      }
-    }
-  `,
   listUserPresaleTickets: gql`
     query listUserPresaleTickets($wallet: String!, $userId: ID!) {
       listUserPresaleTickets(wallet: $wallet, userId: $userId) {
@@ -244,73 +205,6 @@ export const localMutations = {
   `,
 };
 
-export const viewUserByWalletQuery = graphql(queries.viewUserByWallet, {
-  name: 'user',
-  options: props => {
-    log.trace('Running viewUserByWalletQuery with props: ', props);
-    return ({
-      variables: { wallet: typeof window !== 'undefined' && window.web3 && (window.web3.eth.accounts[0] || null) },
-      ssr: false,
-      fetchPolicy: 'no-cache',
-    });
-  },
-});
-
-export const listGamesQuery = graphql(queries.listGames, { name: 'games' });
-
-export const listItemsQuery = graphql(queries.listItems, {
-  name: 'items',
-  skip: props => (props.user && props.user.loading),
-  options: props => {
-    log.trace('Running listItemsQuery with props: ', props);
-    const user = props.user.viewUserByWallet ? props.user.viewUserByWallet : props.user;
-    return ({
-      variables: {
-        language: user.language,
-        userId: user.id,
-      },
-      pollInterval: 30000,
-    });
-  },
-});
-
-export const viewGameBySlugQuery = graphql(queries.viewGameBySlug, {
-  name: 'game',
-  skip: props => !props.slug,
-  options: props => {
-    log.trace('Running viewGameBySlugQuery with props: ', props);
-    return ({
-      variables: {
-        slug: props.slug || (props.game && props.game.slug),
-      },
-    });
-  },
-});
-
-export const listMarketplaceItemsQuery = graphql(queries.listMarketplaceItems, {
-  name: 'marketItems',
-  skip: props => {
-    return (
-      !props.user || !props.games ||
-      (props.user && props.user.loading) || (props.games && props.games.loading)
-    );
-  },
-  options: props => {
-    log.info('Running listMarketplaceItemsQuery with props: ', props);
-    const user = props.user.viewUserByWallet ? props.user.viewUserByWallet : props.user;
-    const games = props.games.listGames ? props.games.listGames : props.games;
-
-    return ({
-      variables: {
-        language: 'en',
-        userId: user.id,
-        gameId: games[0].id,
-        categories: [],
-        sort: null,
-      },
-    });
-  },
-});
 
 export const updateUser = async(id, payload) => {
   const variables = { id, payload: dissoc('__typename', payload) };
